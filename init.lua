bca078e8e6e84f074a6cb43ddd76e16f77eb36c9
Angga Kharisma <anggaputrakharisma@gmail.com>
2024-04-18 15:42:22 +0700

    chore: theme

--- init.lua
+++ init.lua
@@ -393,7 +393,7 @@ end
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'terafox',
+    -- theme = 'nightfox',
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
     disabled_filetypes = {
